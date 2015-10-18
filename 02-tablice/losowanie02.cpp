512029e7d77a69e73a17c32358b796294e085624
Julia Walter <walter.julka@gmail.com>
2015-10-18 18:57:56 +0200

    poprawka komentarzy

---
+++ 02-tablice/losowanie02.cpp
@@ -0,0 +1,32 @@
+/*
+Rozbuduj poprzeni program o operacje modyfikujące elementy tablicy. 
+Po każdej operacji wypisz tablicę na ekran:
+- odwracanie kolejności elementów tablicy
+- przesunięcie tablicy w prawo 
+(element na pozycji 0 jest przesuwany na pozycję 1,
+element na pozycji 1 jest przesuwany na pozycję numer 2, itd. 
+Ostatni element jest wstawiany na pozycję 0)
+- sortowanie tablicy (uporządkowanie elementów rosnąco)
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#define SIZE 10
+
+using std::cout;
+using std::endl;
+
+int main() {
+	
+	int tab[SIZE];
+	srand(time(NULL));
+	
+	for (int i = SIZE; i > 0; i--) {
+		tab[i] = rand() % 100;
+		
+		cout << tab[i] << " ";
+	}
+	
+	
+}
--- 02-tablice/tabliczka.cpp
+++ 02-tablice/tabliczka.cpp
@@ -1,14 +1,3 @@
-/*
-Rozbuduj poprzeni program o następujące operacje modyfikujące elementy tablicy.
-Po każdej operacji wypisz tablicę na ekran, aby sprawdzić, czy operacja została poprawnie wykonana:
-
--  odwracanie kolejności elementów tablicy
-- przesunięcie tablicy w prawo (element na pozycji 0 jest przesuwany na pozycję 1,
-element na pozycji 1 jest przesuwany na pozycję numer 2, itd.
-- Ostatni element jest wstawiany na pozycję 0)
-- sortowanie tablicy (uporządkowanie elementów rosnąco)
-*/
-
 #include <iostream>
 
 #define SIZE 10
@@ -24,7 +13,7 @@ int main() {
         for (int j = 0; j < SIZE; j++) {
             tab[i][j] = (i + 1) * (j + 1);
             cout << tab[i][j] << "\t";
-        }    
+        }
         cout << endl;
     }
 
