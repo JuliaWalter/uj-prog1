15ab27d416ed7b31d182db1a775b9074db6f4098
Julia Walter <walter.julka@gmail.com>
2015-10-24 19:44:37 +0200

    zadanie 2 - wektory

---
+++ 02-tablice/wektory.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	
+	
+	int n{0};
+	cout << "Podaj rozmiar wektorów: ";
+	cin >> n;
+	
+	int v[n], w[n];
+	int tab[n];
+	
+	cout << "wektor V:" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "V[" << i << "] = ";
+		cin >> v[i];
+	}
+	
+	cout << endl << "wektor W:" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "W[" << i << "] = ";
+		cin >> w[i];
+	}
+	
+	int iloczyn_sk{0};
+	
+	cout << endl;
+	cout << "Suma: [";
+	for (int i = 0; i < n; i++) {
+		tab[i] = w[i] + v[i];
+		iloczyn_sk += v[i] * w[i];
+		cout << " " << tab[i]; 
+	}
+	cout << " ]" << endl;
+	cout << "Iloczyn skalarny: " << iloczyn_sk << endl;
+	
+}
