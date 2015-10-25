a65176c7d4c55d1533fb367e27725329f5501093
Julia Walter <walter.julka@gmail.com>
2015-10-25 19:22:40 +0100

    palindrom

---
+++ 02-tablice/palindrom.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	
+	std::string napis;
+	cout << "Wpisz liczbę: ";
+	cin >> napis;
+	
+	bool palindrom{true};
+	for (int i = 0; i < napis.size(); i++) {
+		if (napis[i] != napis[napis.size()-1-i]) {	
+			palindrom = false;
+			break;
+		}
+	}
+	
+	if (palindrom) {
+		cout << "Liczba jest palindromem" << endl;
+	} else {
+		cout << "Liczba nie jest palindromem" << endl;
+	}
+}
